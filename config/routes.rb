579d954e11ea37c249a35b64548eaf89d416eb8b
wws <351483565@qq.com>
2021-12-09 09:06:03 -0500

    new app

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   get 'welcome', to: 'sessions#welcome'
   get 'authorized', to: 'sessions#page_requires_login'
 
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
